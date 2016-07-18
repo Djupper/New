7be803050cca4e71b875e58948f5e7b871fdc07e
Djupper <Djupper@mail.ru>
2016-07-18 16:29:10 +0700

    Create  task4 and end task

--- HomeWorke/ReadandWrite/src/task2.java
+++ HomeWorke/ReadandWrite/src/task2.java
@@ -1,20 +1,18 @@
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.PrintStream;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
 public class task2 {
     public static void main(String[] args) throws IOException {
-        try (PrintWriter writer = new PrintWriter("D:\\IJ\\New\\ReadandWrite\\src\\output.txt");
-        Scanner scanner = new Scanner(
-                new FileInputStream("D:\\IJ\\New\\ReadandWrite\\src\\output.txt"))){
+        try (PrintWriter writer = new PrintWriter("D:\\IJ\\New\\HomeWorke\\ReadandWrite\\output.txt");
+             Scanner scanner = new Scanner(
+                     new FileInputStream("D:\\IJ\\New\\HomeWorke\\ReadandWrite\\input.txt"))) {
             while (scanner.hasNextLine()) {
 
                 writer.println(scanner.nextLine().toUpperCase());
 
             }
-            System.out.println("Fine!");
         }
     }
 }
--- HomeWorke/ReadandWrite/src/task4.java
+++ HomeWorke/ReadandWrite/src/task4.java
@@ -3,6 +3,29 @@
  */
 public class task4 {
     public static void main(String[] args) {
+        /*
+        Написать программу, которая вычленяет из URL
+        адреса имя сервера. Имеется в виду следующее.
+        Для строки вида
+        http://SomeServerName/abcd/dfdf.htm?dfdf=dfdf
+        вычленить SomeServerName
+        • Строка может начинаться не обязательно с http, но
+        и с https или чего-то другого. Но :// есть всегда
+        • Учесть случай, когда после :// больше нет слэша:
+        • http://SomeServerName
+        • Использовать indexOf и substring
+        */
+
+        String url = " http://SomeServerName/abcd/dfdf.htm?dfdf=dfdf";
+        int index = url.indexOf("http://", 1);
+        if (index <= 1) {
+            String[] list = url.substring(8).split("/");
+            for (String v : list)
+                System.out.println(v);
+
+        } else {
+            System.out.println("URL is damage");
+        }
 
     }
 }
