9085ff4949952715e79849db104b97a7346f33cd
Djupper <Djupper@mail.ru>
2016-07-19 15:59:46 +0700

    end task6 ver.2

--- HomeWorke/ReadandWrite/src/task6.java
+++ HomeWorke/ReadandWrite/src/task6.java
@@ -1,4 +1,5 @@
-import sun.util.locale.StringTokenIterator;
+import java.util.Arrays;
+import java.util.FormatFlagsConversionMismatchException;
 
 /**
  * Created by Prog on 19.07.2016.
@@ -9,16 +10,26 @@ public class task6 {
        /* -Разбить строку “1, 2, 3, 4, 5” и получить массив из
         этих чисел и найти их сумму
         -Использовать split и Integer.parseInt*/
-
-        String list = "1,2,3,4,5,6,7";
+        String list = "1,2,3,4,5";
         String[] array = list.split(",");
+        int[] num = new int[array.length];
         int sum = 0;
-        for(int i =0; i<array.length;i++){
-            sum += i;
-            System.out.println(array[i]);
+        for (int i = 0; i < array.length; i++) {
+            num[i] = Integer.parseInt(array[i]);
+            sum += num[i];
         }
         System.out.println(sum);
-
-
     }
 }
+// Используя Лямбды
+        /*
+        String str = "1,2,3,4,5";
+        int[] numArr = Arrays.stream(str.split(",")).mapToInt(Integer::parseInt).toArray();
+        int sum=0;
+        for (int numbers  : numArr){
+            sum += numbers;
+        }
+        System.out.println(sum); //15
+
+    }
+}*/
