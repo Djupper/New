58c0eab8db3984b3ec8f9af9b81c5fe767891ba5
Djupper <Djupper@mail.ru>
2016-07-21 14:02:50 +0700

    task5

--- HomeWorke/ReadandWrite/src/task5.java
+++ HomeWorke/ReadandWrite/src/task5.java
@@ -1,3 +1,5 @@
+import com.sun.org.apache.xalan.internal.xsltc.dom.SimpleResultTreeImpl;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Scanner;
@@ -13,19 +15,16 @@ public class task5 {
         -Использовать цикл и indexOf, который принимает
         начальный индекс, с которого искать
         */
-        try (
-                Scanner scanner = new Scanner(new FileInputStream("D:\\IJ\\New\\HomeWorke\\ReadandWrite\\text.txt"))) {
+        try (Scanner scanner = new Scanner(new FileInputStream("D:\\IJ\\New\\HomeWorke\\ReadandWrite\\text.txt"))) {
 
-            StringBuffer data = new StringBuffer();
-
-            while (scanner.hasNextLine()) {
-
+            StringBuilder data = new StringBuilder();
+            String a = data.append(scanner.hasNextLine()).toString();
+          /*  while (scanner.hasNextLine()) {
                 data.append(scanner.nextLine()).append("\n");
-
-            }
-
+            }*/
 
             System.out.println(data.toString());
+            System.out.println();
 
         }
     }
